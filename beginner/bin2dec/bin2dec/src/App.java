17b451f586f0bab59f5e895d32f874b134e9a8bf
sMeatBear <fcandroidme@gmail.com>
2021-03-01 21:15:52 +0800

    Update App.java

--- beginner/bin2dec/bin2dec/src/App.java
+++ beginner/bin2dec/bin2dec/src/App.java
@@ -1,12 +1,15 @@
-import java.io.InputStream;
 import java.util.Scanner;
+
+import util.MathUtil;
 
 public class App {
     public static void main(String[] args) {
         // 1. get the input from terminal
         Scanner sc = new Scanner(System.in);
-        // 2. sanity check
-        // 3. return decimal result
-
+        // 2. return decimal result
+        int res = MathUtil.bin2Dec(sc.next());
+        sc.close();
+        // 3. print the result
+        System.out.println(res);
     }
 }
