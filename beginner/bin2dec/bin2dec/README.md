5a39a3014bd96709eae6b6d1c39ea77e95023556
sMeatBear <fcandroidme@gmail.com>
2021-03-01 20:23:03 +0800

    Create the mainframe

---
+++ beginner/bin2dec/bin2dec/README.md
@@ -0,0 +1,5 @@
+# Bin2Dec
+Convert binary number input by user to decimal number.
+
+## Constraints:
+Do not use array to contain the binary number input by user.
---
+++ beginner/bin2dec/bin2dec/src/App.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // 1. get the input from terminal
+        Scanner sc = new Scanner(System.in);
+        // 2. sanity check
+        // 3. return decimal result
+
+    }
+}
