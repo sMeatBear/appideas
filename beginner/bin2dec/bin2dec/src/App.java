99d2207cd1ac1f4d40dbd922291df6ad841bb5f4
sMeatBear <fcandroidme@gmail.com>
2021-07-02 17:11:34 +0800

    Change scanner into bufferedreader

--- beginner/bin2dec/bin2dec/src/App.java
+++ beginner/bin2dec/bin2dec/src/App.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import util.MathUtil;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Please input a binary number that is less than 32 bit: ");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please input a binary number that is less than 31 bits: ");
         // 1. get the input from terminal
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 2. return decimal result
-        int res = MathUtil.bin2Dec(sc.next());
-        sc.close();
+        int res = MathUtil.bin2Dec(br.readLine());
+        br.close();
         // 3. print the result
         System.out.println(res == -1 ? "Cannot convert it." : res);
     }
